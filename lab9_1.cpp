bcef38ff78ce64a2a3ad470cb19927e5a56d0d58
Fuuun1024 <worapopfong@gmail.com>
2024-12-13 16:05:22 +0700

    lab9.1_complete

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,10 +2,14 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    double in, per, pay ;
 	cout << "Enter initial loan: ";
+	cin >> in;
 	cout << "Enter interest rate per year (%): ";
+	cin >> per;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +22,39 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	
+	double PrevBalance, Interest, Total, Payment, NewBalance;
+	int i=1;
+	
+	while(true){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << i;
+	
+	if(i==1)PrevBalance = in;
+	else PrevBalance = NewBalance;
+	
+	cout << setw(13) << left << PrevBalance;
+	
+	Interest=PrevBalance*(per/100);
+	cout << setw(13) << left << Interest;
+	
+	Total=PrevBalance+Interest;
+	cout << setw(13) << left << Total;
+	
+	if(Total>pay)
+	Payment = pay;
+	else Payment = Total;
+	cout << setw(13) << left << Payment;
+	
+	NewBalance = Total-Payment;
+	cout << setw(13) << left << NewBalance ;
 	cout << "\n";	
 	
+	    if(NewBalance==0) break;
+	    i++;
+	}
 	return 0;
 }
